678f50da10aabad5bad5881e16afd5bc5083a17f
Cuper Didier <didier.cuper@gmail.com>
2016-06-24 15:33:34 +0000

    Ajout Fichiers

---
+++ js/script.js
@@ -0,0 +1,99 @@
+$(function(){
+
+var question1 = {
+	titre: "Combien y a t-il de nains autour de Blanche Neige ?",
+	reponses: [5, 6, 7, 8],
+	bonneReponse: reponses[2],
+	ReponsesUtilisateur : [],
+	
+};
+
+var question2 = {
+	titre: "Qui est l'inventeur de l'ordinateur ?",
+	reponses: ["Brad Pitt", "Steve Jobs", "Ronald McDonald", "Alan Turing"],
+	bonneReponse: reponses[3],
+	ReponsesUtilisateur : [],
+	
+};
+
+
+var question3 = {
+	titre: "Quelle est la couleur du cheval blanc d'henri IV ?",
+	reponses: ["Jaune", "Blanc", "Gris", "Noir"],
+	bonneReponse: reponses[1],
+	ReponsesUtilisateur : [],
+	
+};
+
+var question4 = {
+	titre: "Qui est l'auteur du livret de la Traviata (La Dame aux camélias) ?",
+	reponses: ["Le père d\'Alexandre Dumas", "Alexandre Dumas", "Le fils d\'Alexandre Dumas", "Le neveu d\'Alexandre Dumas"],
+	bonneReponse: reponses[2],
+	ReponsesUtilisateur : [],
+	
+};
+
+var question5 = {
+	titre: "Combien y a t il de départements français ?",
+	reponses: [98, 99, 100, 101],
+	bonneReponse: reponses[3],
+	ReponsesUtilisateur : [ ],
+	
+};
+
+var question6 = {
+	titre: "Quel est le langage de programmation le plus utilisé ?",
+	reponses: ["C", "C#", "JavaScript", "ActionScript"],
+	bonneReponse: reponses[0],
+	ReponsesUtilisateur : [ ],
+	
+};
+
+var question6 = {
+	titre: "Combien y a t il de pays membres dans UE en 2016 ?",
+	reponses: [20, 24, 28, 35],
+	bonneReponse: reponses[2],
+	ReponsesUtilisateur : [ ],
+	
+};
+
+var question7 = {
+	titre: "Quel pays ne figure pas parmi les membres de UE (2016) ?",
+	reponses: ["Suède", "Norvège", "Danemark", "Finlande"],
+	bonneReponse: reponses[1],
+	ReponsesUtilisateur : [ ],
+	
+};
+
+var question8 = {
+	titre: "Quelle couleur n'est pas présente dans un arc en ciel ?",
+	reponses: ["jaune", "Vert", "Violet", "Marron"],
+	bonneReponse: reponses[3],
+	ReponsesUtilisateur : [ ],
+	
+};
+
+var question9 = {
+	titre: "Quelle couleur n'est pas présente dans un arc en ciel ?",
+	reponses: ["jaune", "Vert", "Violet", "Marron"],
+	bonneReponse: reponses[3],
+	ReponsesUtilisateur : [ ],
+	
+};
+
+var question10 = {
+	titre: "Combien de jours y a t il dans une année bissextile ?",
+	reponses: [364, 365, 366, 367],
+	bonneReponse: reponses[2],
+	ReponsesUtilisateur : [ ],
+	
+};
+
+var questions = [
+	question1, question2, question3, question4, question5, question6, question7, question8, question9, question10
+];
+
+
+
+
+});
