1e8b619c3856fcf2643de69e265b5972cde2698e
Cuper Didier <didier.cuper@gmail.com>
2016-07-06 00:23:32 +0000

    Finalisation exo quizz

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ $(function(){
 	var question2 = {
 		titre: "Qui est l'inventeur de l'ordinateur ?",
 		reponses: ["Brad Pitt", "Steve Jobs", "Ronald McDonald", "Alan Turing"],
-		bonneReponse: 3,
+		bonneReponse: 'Alan Turing',
 		ReponsesUtilisateur : [],
 		
 	};
@@ -23,7 +23,7 @@ $(function(){
 	var question3 = {
 		titre: "Quelle est la couleur du cheval blanc d'henri IV ?",
 		reponses: ["Jaune", "Blanc", "Gris", "Noir"],
-		bonneReponse: 1,
+		bonneReponse: "Blanc",
 		ReponsesUtilisateur : [],
 		
 	};
@@ -31,7 +31,7 @@ $(function(){
 	var question4 = {
 		titre: "Qui est l'auteur du livret de la Traviata (La Dame aux camélias) ?",
 		reponses: ["Le père d\'Alexandre Dumas", "Alexandre Dumas", "Le fils d\'Alexandre Dumas", "Le neveu d\'Alexandre Dumas"],
-		bonneReponse: 2,
+		bonneReponse: "Le fils d\'Alexandre Dumas",
 		ReponsesUtilisateur : [],
 		
 	};
@@ -39,7 +39,7 @@ $(function(){
 	var question5 = {
 		titre: "Combien y a t il de départements français ?",
 		reponses: [98, 99, 100, 101],
-		bonneReponse: 3,
+		bonneReponse: 101,
 		ReponsesUtilisateur : [ ],
 		
 	};
@@ -47,7 +47,7 @@ $(function(){
 	var question6 = {
 		titre: "Quel est le langage de programmation le plus utilisé ?",
 		reponses: ["C", "C#", "JavaScript", "ActionScript"],
-		bonneReponse: 0,
+		bonneReponse: "C",
 		ReponsesUtilisateur : [ ],
 		
 	};
@@ -55,7 +55,7 @@ $(function(){
 	var question7 = {
 		titre: "Combien y a t il de pays membres dans UE en 2016 ?",
 		reponses: [20, 24, 28, 35],
-		bonneReponse: 2,
+		bonneReponse: 28,
 		ReponsesUtilisateur : [ ],
 		
 	};
@@ -63,7 +63,7 @@ $(function(){
 	var question8 = {
 		titre: "Quel pays ne figure pas parmi les membres de UE (2016) ?",
 		reponses: ["Suède", "Norvège", "Danemark", "Finlande"],
-		bonneReponse: 1,
+		bonneReponse: "Norvège",
 		ReponsesUtilisateur : [ ],
 		
 	};
@@ -71,7 +71,7 @@ $(function(){
 	var question9 = {
 		titre: "Quelle couleur n'est pas présente dans un arc en ciel ?",
 		reponses: ["jaune", "Vert", "Violet", "Marron"],
-		bonneReponse: 3,
+		bonneReponse: "Marron",
 		ReponsesUtilisateur : [ ],
 		
 	};
@@ -80,7 +80,7 @@ $(function(){
 	var question10 = {
 		titre: "Combien de jours y a t il dans une année bissextile ?",
 		reponses: [364, 365, 366, 367],
-		bonneReponse: 2,
+		bonneReponse: 366,
 		ReponsesUtilisateur : [ ],
 		
 	};
@@ -88,6 +88,7 @@ $(function(){
 	var questions = [
 		question1, question2, question3, question4, question5, question6, question7, question8, question9, question10
 	];
+
 /* ------------------affichage questions et réponses---------*/
 
 	var activeQuestion = 0;
@@ -97,62 +98,181 @@ $(function(){
 			$("#question").html(questions[activeQuestion].titre);
 				for (var i = 0; i < divReponses.length; i++) {
 					$(divReponses[i]).html(questions[activeQuestion].reponses[i]);
+
 				};
 
-				//return questions[activeQuestion];	
-	}
+	};
 
 	findQuestion(activeQuestion);
 
-	
-	/*var findReponse = function(choixReponse){
-
-		$(divReponses).on("click", function(){
-			var clic = $(event.target);
-			var choix = this.id;
-
-			if ((questions[0].bonneReponse === 7) && (choix === "rep3")) {
-				clic.addClass('vert');
-				console.log(question1.bonneReponse);
-			}
-			else if(choix === 'rep1' || choix === 'rep2' || choix === 'rep4'){
-            	clic.addClass('rouge');
-    		};
-		});
-	};
-
-	findReponse();*/
-
+	$('#rep1').on ("click", function (event) {
 		
+			var choixUtilisateur = $(event.target);
+ 			var tabReponses = questions[activeQuestion].ReponsesUtilisateur;
+			var contenuDiv1 = $('#rep1').text();
 
+					if (contenuDiv1 == questions[activeQuestion].bonneReponse) {
+						if(choixUtilisateur.hasClass('vert') || choixUtilisateur.hasClass('rouge') || tabReponses.length >1){
 
+							alert('vous avez déjà cliqué');
+							
+						}else{
+
+							questions[activeQuestion].ReponsesUtilisateur.push(contenuDiv1,'vert');
+							choixUtilisateur.addClass('vert');	
+							
+						};						
+							  
+					}else{
+
+						if (choixUtilisateur.hasClass('vert') || choixUtilisateur.hasClass('rouge') || tabReponses.length >1) {
+							alert('vous avez déjà cliqué');
+							
+						}else{
+
+							questions[activeQuestion].ReponsesUtilisateur.push(contenuDiv1,'rouge');
+							choixUtilisateur.addClass('rouge');
+							
+						}
+					};
+	});
+  
+	$('#rep2').on ("click", function (event) {
+		var choixUtilisateur = $(event.target);
+		var tabReponses = questions[activeQuestion].ReponsesUtilisateur;
+		var contenuDiv2 = $('#rep2').text();
+
+			if (contenuDiv2 == questions[activeQuestion].bonneReponse) {
+
+				if(choixUtilisateur.hasClass('vert') || choixUtilisateur.hasClass('rouge') || tabReponses.length >1){
+					alert('vous avez déjà cliqué');
+				}else{
+					questions[activeQuestion].ReponsesUtilisateur.push(contenuDiv2,'vert');
+					choixUtilisateur.addClass('vert');
+				};	
+						
+			}else {
+				if(choixUtilisateur.hasClass('vert') || choixUtilisateur.hasClass('rouge') || tabReponses.length >1){
+					alert('vous avez déjà cliqué');
+				}else{
+					questions[activeQuestion].ReponsesUtilisateur.push(contenuDiv2,'rouge');
+					choixUtilisateur.addClass('rouge');
+				};					
+					
+			};
+	});
+
+	$('#rep3').on ("click", function (event) {
+		var choixUtilisateur = $(event.target);
+		var tabReponses = questions[activeQuestion].ReponsesUtilisateur;
+		var contenuDiv3 = $('#rep3').text();
+
+			if (contenuDiv3 == questions[activeQuestion].bonneReponse) {
+
+				if(choixUtilisateur.hasClass('vert') || choixUtilisateur.hasClass('rouge') || tabReponses.length >1){
+					alert('vous avez déjà cliqué');
+				}else{
+					questions[activeQuestion].ReponsesUtilisateur.push(contenuDiv3,'vert');
+					choixUtilisateur.addClass('vert');
+				};
+			}else {
+				if(choixUtilisateur.hasClass('vert') || choixUtilisateur.hasClass('rouge') || tabReponses.length >1){
+					alert('vous avez déjà cliqué');
+				}else{
+					questions[activeQuestion].ReponsesUtilisateur.push(contenuDiv3,'rouge');
+					choixUtilisateur.addClass('rouge');
+				};	
+			};
+	});
+
+	$('#rep4').on ("click", function (event) {
+		var choixUtilisateur = $(event.target);
+		var tabReponses = questions[activeQuestion].ReponsesUtilisateur;
+		var contenuDiv4 = $('#rep4').text();
+
+			if (contenuDiv4 == questions[activeQuestion].bonneReponse) {
+
+				if(choixUtilisateur.hasClass('vert') || choixUtilisateur.hasClass('rouge') || tabReponses.length >1){
+					alert('vous avez déjà cliqué');
+				}else{
+					questions[activeQuestion].ReponsesUtilisateur.push(contenuDiv4,'vert');
+					choixUtilisateur.addClass('vert');
+				};	
+						
+			}else {
+				if(choixUtilisateur.hasClass('vert') || choixUtilisateur.hasClass('rouge') || tabReponses.length >1){
+					alert('vous avez déjà cliqué');
+				}else{
+					questions[activeQuestion].ReponsesUtilisateur.push(contenuDiv4,'rouge');
+					choixUtilisateur.addClass('rouge');
+				};		
+			};
+	});
 
 	/* ------------------affichage questions et reponses suivantes au clic---------*/
 
 
 	$("#next").on('click', function(event){
 
-		if (activeQuestion < 9) {
+		var divReponsesId = ['#rep1', '#rep2', '#rep3', '#rep4'];
+		
+			
+		if(questions[activeQuestion].ReponsesUtilisateur.length == 0){
+			
+				alert('Merci de choisir une réponse');
+				
+		}else if(activeQuestion >= 9){
+		
+					alert('Le jeu est terminé');
+				
+		}else if (activeQuestion < 10) {
+			
 				activeQuestion++ ;
-				//console.log(activeQuestion);
+				$(divReponses).removeClass('rouge vert');
+				
+				for (var i = 0; i < questions[activeQuestion].reponses.length; i++) {
+					
+					if (questions[activeQuestion].ReponsesUtilisateur[0] == questions[activeQuestion].reponses[i]) {
+						
+						$(divReponsesId[i]).addClass(questions[activeQuestion].ReponsesUtilisateur[1]);
+					};
+				};
 		} ;
+
 		findQuestion(activeQuestion);
-	//	findReponse();
-		$(divReponses).removeClass('rouge vert');
 
 	});
+	
 	/* ------------------affichage questions et reponses précedentes au clic---------*/
 
 	$("#prev").on('click', function(event){
-	    			
-		if (activeQuestion > 0) {
-				activeQuestion-- ;
-		} ;
 
-		findQuestion(activeQuestion);
-		$(divReponses).removeClass('rouge vert');
+	  var divReponsesId = ['#rep1', '#rep2', '#rep3', '#rep4'];	
+	  
+	  if(questions[activeQuestion].ReponsesUtilisateur.length == 0){
+			
+				alert('Merci de choisir une réponse');
+				
+		}else if (activeQuestion > 0) {
+			
+			activeQuestion-- ;
+			
+			
+			for (var i = 0; i < questions[activeQuestion].reponses.length; i++) {
+				if (questions[activeQuestion].ReponsesUtilisateur[0] == questions[activeQuestion].reponses[i]) {
+
+					if (divReponses.hasClass('vert') || divReponses.hasClass('rouge')) {
+
+						$(divReponses).removeClass('vert rouge');
+
+						$(divReponsesId[i]).addClass(questions[activeQuestion].ReponsesUtilisateur[1]);
+					};
+				};
+			};
+		};
+
+		findQuestion(activeQuestion);		
 
 	});
 
 });
-		
